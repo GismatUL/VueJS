b8e581619b601c61f5565953a2adaab34918f34c
EyCoder <qmammadov539@gmail.com>
2023-11-11 00:23:42 +0400

    trying to test vue js

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+require('./bootstrap');
+
+import { createApp } from 'vue';
+import MainApp from './components/mainApp.vue';
+
+const app = createApp(MainApp);
+
+app.component('mainApp', MainApp);
+
+app.mount('#app');
---
+++ resources/js/components/mainApp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>This is my first Vue project</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    // component definition
+};
+</script>
+
+<style>
+/* styles here */
+</style>
